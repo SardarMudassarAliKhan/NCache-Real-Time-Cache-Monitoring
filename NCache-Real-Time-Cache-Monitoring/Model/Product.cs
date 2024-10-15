ceed287829129916bf00c24615760c4614050cce
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-10-15 21:21:33 +0500

    Add project files.

---
+++ NCache-Real-Time-Cache-Monitoring/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace NCache_Real_Time_Cache_Monitoring.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+}
